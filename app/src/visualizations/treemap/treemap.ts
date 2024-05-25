a82e3b34e06107a6214b72bc145ea9db046687b3
blasvicco@gmail.com <blasvicco@gmail.com>
2024-05-25 16:39:04 +0200

    Improve: Trying to clean up a little bit the TreemapVisualization upload method.

--- app/src/visualizations/treemap/treemap.ts
+++ app/src/visualizations/treemap/treemap.ts
@@ -9,7 +9,7 @@ import { select, Selection } from 'd3-selection'
 
 // App imports
 import { formatType, generateUid } from 'common/util'
-import { VisDefinition, VisData, Row, Cell } from 'types/looker'
+import { VisDefinition, VisData, VisQueryResponse, Row, Cell } from 'types/looker'
 import './treemap.css'
 
 /*********************/
@@ -22,6 +22,7 @@ type Datum = Row | Map<string, Datum>
 interface TreemapVisualization extends VisDefinition {
   frame?: Selection<SVGGElement, unknown, null, undefined>
   focusRing?: Selection<SVGRectElement, unknown, null, undefined>
+  __hasError: (queryResponse: VisQueryResponse) => boolean
 }
 
 // constant declaration
@@ -55,6 +56,24 @@ const buildHierarchy = (data: VisData, dimensions: any[]) => {
       return children instanceof Map ? [...children.entries()] : null
     }
   )
+}
+
+const getCell = (
+  queryResponse: VisQueryResponse,
+  position: number,
+  row: Row
+) => {
+  const {
+    fields: { measures, table_calculations },
+    pivots = []
+  } = queryResponse
+  if (table_calculations[position]) {
+    return row[table_calculations[position].name]
+  } else if (pivots[position]) {
+    return row[measures[0].name][pivots[position].key]
+  } else if (measures[position]) {
+    return row[measures[position].name]
+  }
 }
 
 const weightedMean = <T>(
@@ -136,7 +155,28 @@ const vis: TreemapVisualization = {
     }
   },
   // object methods
-  create: function (element, settings) {
+  __hasError(queryResponse: VisQueryResponse) {
+    const {
+      fields: { dimensions, measures, table_calculations },
+      pivots = []
+    } = queryResponse
+    const error = { message: '', title: '' }
+
+    this.clearErrors!()
+    if (dimensions.length === 0) {
+      error.title = 'Dimensions Required'
+      error.message= 'Please include at least one dimension.'
+    } else if (!measures[0] && !table_calculations[0]) {
+      error.title = 'Primary Measure Required'
+      error.message = 'Please include at least one un-pivoted measure or table calculation.'
+    }
+    if (error.title) {
+      this.addError!(error)
+      return true
+    }
+    return false
+  },
+  create(element, settings) {
     this.frame = select(element)
       .append('svg')
       .attr('width', '100%')
@@ -144,206 +184,125 @@ const vis: TreemapVisualization = {
       .append('g')
       .attr('transform', `translate(${MARGIN}, ${MARGIN})`)
   },
-  update: function (data, element, config, queryResponse, details) {
-    this.clearErrors!()
-
+  update(data, element, config, queryResponse, details) {
+    // const declaration
     const { clientWidth: width, clientHeight: height } = element
-
     const {
-      fontSize,
-      palette,
-      gradient,
-      gradientMin,
-      gradientMax,
       displayMeasure,
+      fontSize,
+      hideClippedLabels,
+      gradient,
+      gradientMax,
+      gradientMin,
       measureFormat,
-      hideClippedLabels
+      palette
     } = config
-
     const {
       fields: { dimensions, measures, table_calculations },
       pivots = []
     } = queryResponse
 
-    if (dimensions.length === 0) {
-      this.addError!({
-        title: 'Dimensions Required',
-        message: 'Please include at least one dimension.'
-      })
-      return
-    }
-    if (!measures[0] && !table_calculations[0]) {
-      this.addError!({
-        title: 'Primary Measure Required',
-        message:
-          'Please include at least one un-pivoted measure or table calculation.'
-      })
-      return
-    }
+    // call error check and exit if any
+    if (this.__hasError(queryResponse)) return
 
-    const isWeighted = table_calculations[1] || pivots[1] || measures[1]
-
-    const getPrimaryCell = (row: Row) => {
-      if (table_calculations[0]) {
-        return row[table_calculations[0].name]
-      } else if (pivots[0]) {
-        return row[measures[0].name][pivots[0].key]
-      } else if (measures[0]) {
-        return row[measures[0].name]
-      }
-    }
-
-    const getSecondaryCell = (row: Row) => {
-      if (table_calculations[1]) {
-        return row[table_calculations[1].name]
-      } else if (pivots[1]) {
-        return row[measures[0].name][pivots[1].key]
-      } else if (measures[1]) {
-        return row[measures[1].name]
-      }
-    }
-
-    const formatDimension = (value: string | null) =>
-      value === null ? '∅' : value
-    const formatPrimaryMeasure = formatType(
-      measureFormat || measures[0].value_format
-    )
-
-    const activateCell = (event: Event, d: HierarchyNode<[string, Datum]>) => {
-      const links = d
-        .leaves()
-        .map((leaf) => leaf.data[1] as Row)
-        .map((row) => getPrimaryCell(row) as Cell)
-        .flatMap((cell) => cell.links || [])
-        .map((link) => new URL(link.url, window.location.href))
-
-      if (links.length === 0) {
-        return
-      }
-
-      const url = links.reduce((acc, url) => {
-        acc.searchParams.forEach((value, key) => {
-          if (url.searchParams.get(key) !== value) {
-            acc.searchParams.delete(key)
-          }
-        })
-        return acc
-      })
-      LookerCharts.Utils.openDrillMenu({
-        event,
-        links: [
-          {
-            type: 'drill',
-            label: `Show all for ${formatDimension(d.data[0])}`,
-            type_label: 'Explore',
-            url: url.pathname + url.search
-          }
-        ]
-      })
-    }
-
-    const root = buildHierarchy(data, dimensions)
-      .sum(([_, datum]) =>
-        datum instanceof Map ? 0 : getPrimaryCell(datum).value
-      )
-      .sort((a, b) => b.value! - a.value!)
-
+    const isWeighted = Boolean(table_calculations[1] || pivots[1] || measures[1])
     const scaleSecondaryMeasure = scaleSequential(
       interpolateRgbBasis(gradient || DEFAULT_GRADIENT)
     )
-
     if (isWeighted) {
-      const domain = extent(data.map((row) => getSecondaryCell(row).value))
+      const domain = extent(data.map((row) => getCell(queryResponse, 1, row).value))
       scaleSecondaryMeasure.domain([
         gradientMin == null ? domain[0] : gradientMin,
         gradientMax == null ? domain[1] : gradientMax
       ])
     }
 
-    const color = scaleOrdinal<string>()
-      .domain(root.children!.map((d) => d.data[0]))
-      .range(palette || DEFAULT_PALETTE)
-
-    const boxFill = (d: HierarchyNode<[string, Datum]>) =>
-      isWeighted
-        ? scaleSecondaryMeasure(
-            weightedMean(
-              d.leaves().map((leaf) => leaf.data[1] as Row),
-              (row) => getSecondaryCell(row).value,
-              (row) => getPrimaryCell(row).value
-            )
-          )
-        : color(d.ancestors().slice(-2, -1)[0].data[0])
-
-    const textFill = (d: HierarchyNode<[string, Datum]>) =>
-      hsl(boxFill(d)).l > 0.5 ? '#454545' : '#FFFFFF'
-
     const layout = treemap<[string, Datum]>()
       .size([width - 2 * MARGIN, height - 2 * MARGIN])
-      .paddingTop((d) => d.depth && fontSize * 1.5 * Math.pow(1.4, d.height))
+      .paddingTop((dNode) => dNode.depth && fontSize * 1.5 * Math.pow(1.4, dNode.height))
       .round(true)
-
+    const root = buildHierarchy(data, dimensions)
+      .sum(([_, datum]) => datum instanceof Map
+          ? 0
+          : getCell(queryResponse, 0, datum).value
+      )
+      .sort((a, b) => b.value! - a.value!)
     const uidIndex = new Map<HierarchyNode<unknown>, number>()
-
     const nodes = this.frame!.selectAll('.node')
       .data(
         layout(root)
           .descendants()
-          .filter((d) => d.depth > 0)
+          .filter((dNode) => dNode.depth > 0)
       )
       .join((enter) => {
         const group = enter
           .append('g')
           .attr('class', 'node')
-          .attr('data-uid', (d) => {
+          .attr('data-uid', (dNode) => {
             const uid = generateUid()
-            uidIndex.set(d, uid)
+            uidIndex.set(dNode, uid)
             return uid
           })
 
         group.append('rect')
           .attr('class', 'box')
-          .attr('id', (d) => `box-${uidIndex.get(d)}`)
+          .attr('id', (dNode) => `box-${uidIndex.get(dNode)}`)
         group.append('clipPath')
-          .attr('id', (d) => `clip-${uidIndex.get(d)}`)
+          .attr('id', (dNode) => `clip-${uidIndex.get(dNode)}`)
           .append('use')
-          .attr('href', (d) => `#box-${uidIndex.get(d)}`)
+          .attr('href', (dNode) => `#box-${uidIndex.get(dNode)}`)
           .attr('stroke-width', '0.25em')
         group.append('text')
           .attr('class', 'label')
           .attr(
             'clip-path',
-            (_d, i, nodes) =>
-              `url(#clip-${nodes[i].parentElement!.dataset.uid})`
+            (_d, i, nodes) => `url(#clip-${nodes[i].parentElement!.dataset.uid})`
           )
         return group
       })
       .attr('class', 'node')
-      .attr('transform', (d) => `translate(${d.x0}, ${d.y0})`)
+      .attr('transform', (dNode) => `translate(${dNode.x0}, ${dNode.y0})`)
 
+    const color = scaleOrdinal<string>()
+      .domain(root.children!.map((dNode) => dNode.data[0]))
+      .range(palette || DEFAULT_PALETTE)
+    const boxFill = (dNode: HierarchyNode<[string, Datum]>) => isWeighted
+      ? scaleSecondaryMeasure(
+          weightedMean(
+            dNode.leaves().map((leaf) => leaf.data[1] as Row),
+            (row) => getCell(queryResponse, 1, row).value,
+            (row) => getCell(queryResponse, 0, row).value
+          )
+        )
+      : color(dNode.ancestors().slice(-2, -1)[0].data[0])
     nodes.selectAll('.box')
-      .data((d) => d)
-      .attr('width', (d) => d.x1 - d.x0)
-      .attr('height', (d) => d.y1 - d.y0)
+      .data((dNode) => dNode)
+      .attr('width', (dNode) => dNode.x1 - dNode.x0)
+      .attr('height', (dNode) => dNode.y1 - dNode.y0)
       .attr('fill', boxFill)
 
+    const textFill = (dNode: HierarchyNode<[string, Datum]>) => hsl(boxFill(dNode)).l > 0.5
+      ? '#454545'
+      : '#FFFFFF'
+    const fDimension = (value: string | null) => value === null ? '∅' : value
+    const fPrimaryMeasure = displayMeasure
+      ? formatType(
+        measureFormat || measures[0].value_format
+      )
+      : (_: any) => ''
     nodes.selectAll<SVGTextElement, Datum>('text.label')
-      .data((d) => d)
-      .attr('font-size', (d) => fontSize * Math.pow(1.4, d.height))
+      .data((dNode) => dNode)
+      .attr('font-size', (dNode) => fontSize * Math.pow(1.4, dNode.height))
       .attr('x', fontSize * 0.5)
       .attr('y', '1.1em')
       .attr('fill', textFill)
       .text(
-        (node) =>
-          `${formatDimension(node.data[0])} ${
-            displayMeasure ? `(${formatPrimaryMeasure(node.value!)}) ` : ''
-          }`
+        (node) => `${fDimension(node.data[0])} ${fPrimaryMeasure(node.value!)}`
       )
       .attr('class', (_, i, nodes) => {
         const element = nodes[i]
         const label = element.getBoundingClientRect()
-        const rect = element
-          .parentElement?.querySelector('.box')?.getBoundingClientRect()
+        const rect = element.parentElement!.querySelector('.box')?.getBoundingClientRect()
         const occluded = rect &&
           hideClippedLabels &&
           (label.left < rect.left ||
@@ -353,32 +312,64 @@ const vis: TreemapVisualization = {
         return clsx('label', { occluded })
       })
 
+    const activateCell = (event: Event, dNode: HierarchyNode<[string, Datum]>) => {
+      const links = dNode
+        .leaves()
+        .map((leaf) => leaf.data[1] as Row)
+        .map((row) => getCell(queryResponse, 0, row) as Cell)
+        .flatMap((cell) => cell.links || [])
+        .map((link) => new URL(link.url, window.location.href))
+
+      // if no links then exit
+      if (links.length === 0) return
+
+      // TODO: Ask Jeff or Jacob about this code
+      // acc it is not initialized so acc.searchParams should be empty
+      const url = links.reduce((acc, url) => {
+        acc.searchParams.forEach((value, key) => {
+          if (url.searchParams.get(key) !== value) {
+            acc.searchParams.delete(key)
+          }
+        })
+        return acc
+      })
+
+      LookerCharts.Utils.openDrillMenu({
+        event,
+        links: [{
+          label: `Show all for ${fDimension(dNode.data[0])}`,
+          type: 'drill',
+          type_label: 'Explore',
+          url: `${url.pathname}${url.search}`
+        }]
+      })
+    }
     nodes
       .attr('role', 'button')
       .style('cursor', 'pointer')
       .attr('tabindex', 0)
-      .attr('aria-label', (d) => d.data[0])
-      .on('focus', (_event, d) => {
+      .attr('aria-label', (dNode) => dNode.data[0])
+      .on('focus', (_event, dNode) => {
         this.focusRing = this.frame!.append('rect')
           .attr('class', 'focus-ring')
           .attr('fill', 'none')
-          .attr('x', d.x0)
-          .attr('y', d.y0)
-          .attr('width', d.x1 - d.x0)
-          .attr('height', d.y1 - d.y0)
+          .attr('x', dNode.x0)
+          .attr('y', dNode.y0)
+          .attr('width', dNode.x1 - dNode.x0)
+          .attr('height', dNode.y1 - dNode.y0)
       })
       .on('blur', () => {
         this.focusRing?.remove()
       })
       .on('click', activateCell)
-      .on('keydown', (event: KeyboardEvent, d) => {
+      .on('keydown', (event: KeyboardEvent, dNode) => {
         if (event.code === 'Space' || event.code === 'Enter') {
           activateCell(
             Object.assign(event, {
-              pageX: d.x0 + (d.x1 - d.x0) / 2,
-              pageY: d.y0 + (d.y1 - d.y0) / 2
+              pageX: dNode.x0 + (dNode.x1 - dNode.x0) / 2,
+              pageY: dNode.y0 + (dNode.y1 - dNode.y0) / 2
             }),
-            d
+            dNode
           )
         }
       })
